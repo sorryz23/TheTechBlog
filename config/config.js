caf2d5e3c269f6f43c31e512c09bfd9948fed8d6
sorryz23 <sorryz23@gmail.com>
2023-02-02 11:02:35 -0500

    file structure complete, DB done, and config for env.

---
+++ config/config.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequilize');
+
+require('dontenv').config();
+
+// create connection to the database
+const sequelize = process.env.JAWSDB_URL
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+module.exports = sequelize;
