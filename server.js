6c2c4d0746c0eba92eced513a748fd2925b2176f
sorryz23 <sorryz23@gmail.com>
2023-02-02 11:34:25 -0500

    added json, and finished server code. Started handlebars

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const path = reuire('path');
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
@@ -24,3 +24,23 @@ const sess = {
         db: sequelize
     })
 };
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    sequelize.sync({ force: false});
+});
